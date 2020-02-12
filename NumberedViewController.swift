c2672de9feaf00a0247231839a08e16c6d9260a6
kendra rayne <raynekendra@gmail.com>
2020-02-11 22:27:23 -0500

    Middle of colorscreens

---
+++ NumberedViewController.swift
@@ -0,0 +1,30 @@
+//
+//  NumberedViewController.swift
+//  screens(iOS)
+//
+//  Created by Kendra McVay on 2/11/20.
+//  Copyright © 2020 Kendra McVay. All rights reserved.
+//
+
+import UIKit
+
+class NumberedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  screens(iOS)
+//
+//  Created by Kendra McVay on 2/11/20.
+//  Copyright © 2020 Kendra McVay. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
